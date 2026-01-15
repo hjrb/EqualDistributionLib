8a970fac5c59bebbfdba56d810dbd42eb899dbad
Reister Hansjoerg <ret2fe@bosch.com>
2026-01-15 11:25:59 +0100

    Add project files.

---
+++ EqualDistributionLib/EqualDistribution.cs
@@ -0,0 +1,193 @@
+﻿using System.Numerics;
+
+namespace EqualDistributionLib;
+
+
+public class EqualDistribution
+{
+
+	public class BinItem<TPropertyValue>
+		where TPropertyValue : notnull, IEquatable<TPropertyValue>
+	{
+		public required TPropertyValue PropertyValue { get; init; }
+		public int Count { get; set; }
+		public override string ToString() => $"{PropertyValue}=>{Count}";
+		public override int GetHashCode() => HashCode.Combine(PropertyValue, Count);
+		public override bool Equals(object? obj) => obj is BinItem<TPropertyValue> other && other.PropertyValue.Equals(this.PropertyValue) && other.Count == this.Count;
+
+	}
+
+
+	/// <summary>
+	/// Take a list of items and distribute them equally into bins 
+	/// The correlation item to bin is determined the Func keySelector.
+	/// If there is no bin for an item, it is added to the bin with the least items.
+	/// Then the items are redistributed to achieve equal distribution across bins.
+	/// Notes: this approach will 
+	/// 1.) add the items in the source items collection to the bins if they are missing. 2.)
+	/// 2.) actually move the items
+	/// This is well suited if the items are already in memory
+	/// </summary>
+	/// <typeparam name="TItem">the item type</typeparam>
+	/// <typeparam name="TPropertyValue">the type of the Property</typeparam>
+	/// <param name="items">source of items</param>
+	/// <param name="PropertySelector">function selecting the Property for each item to sort the item into the bin</param>
+	/// <param name="bins">the bins to distribute the items into. These can be empty at the start or preloaded</param>
+	/// <returns>the enumeration of the items that have been assigned to bin with deviating key. Those items must be updated to assign the key of the bin to the Property to persist and equal distribution</returns>
+	public static async Task<IEnumerable<(TItem Item, TPropertyValue Property)>>
+		DistributeEquallyAsync<TItem, TPropertyValue>(
+			IEnumerable<TItem> items,
+			Func<TItem, TPropertyValue> PropertySelector,
+			Dictionary<TPropertyValue, ICollection<TItem>> bins,
+			IEqualityComparer<TItem>? equalityComparer = null
+		)
+		where TPropertyValue : notnull, IEquatable<TPropertyValue>
+	{
+
+		equalityComparer ??= EqualityComparer<TItem>.Default;
+
+		// Phase 1 add the items not assigned to any bin
+		foreach (var item in items)
+		{
+			var key = PropertySelector(item);
+			// get the bin for the items key
+			if (bins.ContainsKey(key))
+			{
+				// add the item to the bin if it not alreay in the bin
+				if (!bins[key].Contains(item, equalityComparer))
+				{
+					bins[key].Add(item);
+				}
+			}
+			else
+			{
+				// get bin with least amount of items
+				// TODO we could have a parameter to specify how to handle this case
+				var targetBin = bins.OrderBy(b => b.Value.Count).First();
+				targetBin.Value.Add(item);
+			}
+		}
+		var movedItems=new List<(TItem Item, TPropertyValue Property)>();
+		await DistributeEquallyAsync(bins.Select(a=>new BinItem<TPropertyValue>() { PropertyValue=a.Key, Count=a.Value.Count }), async (count, from, to) =>
+		{
+			// move 'count' items from 'from' bin to 'to' bin
+			var sourceBin = bins[from];
+			var targetBin = bins[to];
+			var moved=0;
+			for (var i = 0; i < count; i++)
+			{
+				if (sourceBin.Count == 0) break; // should not happen
+				var itemToMove = sourceBin.First();
+				sourceBin.Remove(itemToMove);
+				targetBin.Add(itemToMove);
+				++moved;
+			}
+			return await Task.FromResult(moved);
+		});
+		return await Task.FromResult(movedItems);
+	}
+
+	/// <summary>
+	/// Redistributes items among bins so that each bin has as close to an equal number of items as possible, moving items
+	/// asynchronously between bins as needed.
+	/// The method attempts to minimize the difference in item counts between bins by repeatedly invoking
+	/// the provided moveItem delegate. The actual number of items moved depends on the initial distribution. The method
+	/// does not guarantee perfect balance if the total number of items cannot be evenly divided among bins.</remarks>
+	/// This approach does not actuall move the items but call the delegate moveItem so the caller can implement the actual move logic.
+	/// This is well suited if the items have not been loaded to memory yet, e.g. in database scenario
+	/// Example:
+	/// You have DB table processingTable with a column "processingDate". You have a huge number of items and want to distribute the processing across n-dates equally.
+	/// <![CDATA[
+	///		var grouped=processingTable.GroupBy(a=>a.processingDate).Select(g=>new {g.Key, Count=g.Count()}).ToArray().Select(a=>new BinItem() {Key=a.Key, Count=a.Count}).ToArray()
+	///		var alreadyMoved=new HashSet<int>();
+	///			await EqualDistribution(grouped, (from,to)=> {
+	///				var itemToMove=processingTable.Where(a=>a.processingDate==from && !alreadyMoved.Contains(item.ID)).First();
+	///				itemToMove.processingDate=to; // moves the processingDate
+	///				alreadyMoved.Add(itemToMove.ID);
+	///			}
+	///		);
+	///		await dbContext.SaveChangesAsync();
+	///	]]>
+	/// <typeparam name="TPropertyValue">The type that identifies each bin. Must be non-null and support equality comparison.</typeparam>
+	/// <param name="bins">A collection of tuples, each containing a bin identifier and the current item count for that bin. The method
+	/// attempts to balance the item counts across these bins.</param>
+	/// <param name="moveItem">An asynchronous delegate that moves a single item from one bin to another. The first parameter is the source bin
+	/// identifier, and the second is the target bin identifier.</param>
+	/// <returns>A task that represents the asynchronous redistribution operation.</returns>
+	public static async Task
+		DistributeEquallyAsync<TPropertyValue>(
+			IEnumerable<BinItem<TPropertyValue>> bins,
+			Func<int, TPropertyValue, TPropertyValue, Task<int>> moveItem
+		)
+		where TPropertyValue : notnull, IEquatable<TPropertyValue>
+	{
+		var totalItems = bins.Sum(b => b.Count);
+		var hiPerBin=(int)Math.Round(totalItems / (double)bins.Count(), MidpointRounding.ToPositiveInfinity);
+		var lowPerBin = (int)Math.Round(totalItems / (double)bins.Count(), MidpointRounding.ToNegativeInfinity);
+		var totalMoved=0;
+		for (var i = 0; i < bins.Count(); i++)
+		{
+			var bin = bins.ElementAt(i);
+			var movedItems = 0;
+			while (bin.Count > hiPerBin)
+			{
+				// this should do all required moves all at once.
+				// but as this is in an outer data source things might have changed since the input bin distribution has been created
+				// find target bin as the bin that currently has the least number of items
+				var targetBin = bins.OrderBy(b => b.Count).First();
+				if (object.Equals(targetBin, bin)) break; // no more items to move
+				var toMove=Math.Min(hiPerBin-targetBin.Count, bin.Count-lowPerBin);
+				if (toMove==0) {
+					throw new Exception("Nothing can be moved");
+				}
+				var moved= await DoMove(moveItem, bin, targetBin, toMove);
+				movedItems +=moved;
+				if (moved<=0) {
+					break; // nothing moved - can't have optimum here
+				}
+			}
+			while (bin.Count < lowPerBin)
+			{
+				// this should do all required moves all at once
+				// but as this is in an outer data source things might have changed since the input bin distribution has been created
+				// find target bin as the bin that currently has the maximum number of items
+				var sourceBin = bins.OrderByDescending(b => b.Count).First();
+				if (object.Equals(sourceBin, bin)) break; // no more items to move
+				var toMove = Math.Min(lowPerBin - bin.Count, sourceBin.Count - lowPerBin);
+				if (toMove==0) {
+					throw new Exception("Nothing can be moved");
+				}
+				var moved= await DoMove(moveItem, sourceBin, bin, toMove);
+				movedItems +=moved;
+				if (moved<=0) {
+					break; // nothing moved
+				}
+			}
+			totalMoved += movedItems;
+		}
+	}
+
+	private static async Task<int> DoMove<TPropertyValue>(
+		Func<int, TPropertyValue, TPropertyValue, Task<int>> moveItem,
+		BinItem<TPropertyValue> source,
+		BinItem<TPropertyValue> targetBin,
+		int toMove
+	) where TPropertyValue : notnull, IEquatable<TPropertyValue>
+	{
+		var moved=await moveItem(toMove,source.PropertyValue, targetBin.PropertyValue);
+		source.Count -= moved;
+		targetBin.Count += moved;
+		return moved;
+	}
+
+	public static void DumpBins<TItem, TPropertyValue>(Dictionary<TPropertyValue, ICollection<TItem>> bins, Action<string> dumper)
+	where TPropertyValue : notnull, IEquatable<TPropertyValue>
+	{
+		dumper($"Bins: {bins.Count}, Keys: {string.Join(", ", bins.Keys.Order())}, Total Items in bins: {bins.Values.Sum(a => a.Count)}, Average: {bins.Values.Sum(a => a.Count) / (double)bins.Count}");
+		foreach (var bin in bins.OrderBy(a=>a.Key))
+		{
+			dumper($"Bin {bin.Key}: {bin.Value.Count} items");
+		}
+
+	}
+}
---
+++ EqualDistributionTest/TestEqualDistribution.cs
@@ -0,0 +1,130 @@
+﻿using EqualDistributionLib;
+
+using static EqualDistributionLib.EqualDistribution;
+
+namespace EqualDistributionTest;
+
+[TestClass]
+public sealed class TestEqualDistribution
+{
+
+	class TestItem(int key, string value) { public int key=key; public string value=value; }
+	[AssemblyInitialize]
+	public static void AssemblyInit(TestContext context)
+	{
+		// This method is called once for the test assembly, before any tests are run.
+	}
+
+	[AssemblyCleanup]
+	public static void AssemblyCleanup()
+	{
+		// This method is called once for the test assembly, after all tests are run.
+	}
+
+	[ClassInitialize]
+	public static void ClassInit(TestContext context)
+	{
+		// This method is called once for the test class, before any tests of the class are run.
+	}
+
+	[ClassCleanup]
+	public static void ClassCleanup()
+	{
+		// This method is called once for the test class, after all tests of the class are run.
+	}
+
+
+	[TestInitialize]
+	public void TestInit()
+	{
+	}
+
+	[TestCleanup]
+	public void TestCleanup()
+	{
+		// This method is called after each test method.
+	}
+
+	[TestMethod]
+	public async Task TestMethodDistributeAsync1()
+	{
+		List<TestItem> items=[];
+		Dictionary<int, ICollection<TestItem>> bins=new();
+		int notInBin=0;
+		items = Enumerable.Range(1, 1023).Select(i => new TestItem(Random.Shared.Next(0, 16), $"Item {i}")).ToList();
+		// add some more randomness
+		for (var i = 0; i < Random.Shared.Next(200, 500); ++i)
+		{
+			items.ElementAt(Random.Shared.Next(0, items.Count)).key = Random.Shared.Next(0, 16);
+		}
+		bins = items
+			.Where(a => a.key < 10)
+			.GroupBy(a => a.key)
+			.ToDictionary(g => g.Key, g => (ICollection<TestItem>)g.ToList());
+		notInBin = items.Count(a => !bins.ContainsKey(a.key));
+		EqualDistribution.DumpBins(bins, Console.WriteLine);
+		var result=await EqualDistribution.DistributeEquallyAsync(items, a => a.key, bins, null);
+		var totalItems=bins.Values.Sum(b=>b.Count);
+		var targetPerBin=totalItems/bins.Count;
+		EqualDistribution.DumpBins(bins, Console.WriteLine);
+		foreach (var bin in bins)
+		{
+			Assert.IsTrue(bin.Value.Count == targetPerBin || bin.Value.Count == targetPerBin + 1);
+		}
+		var mustUpdateKey=result.ToList();
+		Assert.IsLessThanOrEqualTo(notInBin, mustUpdateKey.Count());
+		Console.WriteLine($"Items not in bin: {notInBin}, items to update key: {mustUpdateKey.Count()}");
+
+	}
+	[TestMethod]
+	public async Task TestMethodDistributeAsync2()
+	{
+		List<TestItem> items=[];
+		Dictionary<int, ICollection<TestItem>> bins=new();
+		var notInBin=0;
+		items = Enumerable.Range(1, 1023).Select(i => new TestItem(Random.Shared.Next(0, 10), $"Item {i}")).ToList();
+		// add some more randomness
+		for (var i = 0; i < Random.Shared.Next(200, 500); ++i)
+		{
+			items.ElementAt(Random.Shared.Next(0, items.Count)).key = Random.Shared.Next(0, 10);
+		}
+		bins = items
+			.GroupBy(a => a.key)
+			.ToDictionary(g => g.Key, g => (ICollection<TestItem>)g.ToList());
+		notInBin = items.Count(a => !bins.ContainsKey(a.key));
+		EqualDistribution.DumpBins(bins, Console.WriteLine);
+		var result=await EqualDistribution.DistributeEquallyAsync(items, a => a.key, bins, null);
+		var totalItems=bins.Values.Sum(b=>b.Count);
+		var lowPerBin=Math.Round(totalItems/(double)bins.Count, MidpointRounding.ToNegativeInfinity);
+		var highPerBin=Math.Round(totalItems/(double)bins.Count, MidpointRounding.ToPositiveInfinity);
+		EqualDistribution.DumpBins(bins, Console.WriteLine);
+		foreach (var bin in bins)
+		{
+			Assert.IsTrue(bin.Value.Count == lowPerBin || bin.Value.Count == highPerBin);
+		}
+		var mustUpdateKey=result.ToList();
+		Console.WriteLine($"Items not in bin: {notInBin}, items to update key: {mustUpdateKey.Count()}");
+
+	}
+	[TestMethod]
+public async Task TestMethodDistributeAsync3()
+{
+	var binItems=new List<BinItem<int>>()
+	{
+		new(){ PropertyValue=0, Count=114 },
+		new(){ PropertyValue=1, Count=100 },
+		new(){ PropertyValue=2, Count=97 },
+		new(){ PropertyValue=3, Count=98 },
+		new(){ PropertyValue=4, Count=120 },
+		new(){ PropertyValue=5, Count=94 },
+		new(){ PropertyValue=6, Count=99 },
+		new(){ PropertyValue=7, Count=101 },
+		new(){ PropertyValue=8, Count=100 },
+		new(){ PropertyValue=9, Count=100 },
+	};
+	await EqualDistribution.DistributeEquallyAsync(binItems, async (count, from, to)=>{
+		return await Task.FromResult(count);
+		});
+
+}
+}
